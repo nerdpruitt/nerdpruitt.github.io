6972d1590bc815f555ce9ce8e40a928e13b48efc
Nick Pruitt <pruitt.nick@gmail.com>
2021-06-05 20:51:50 -0500

    Code split CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: ['./src/js/main.js', './src/scss/index.scss'],
+    entry: ['./src/js/main.js', '/src/scss/home.scss', './src/scss/main.scss'],
     output: {
         path: path.resolve(__dirname, 'src/dist'),
         filename: 'js/main.js'
@@ -12,8 +12,21 @@ module.exports = {
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].css',
+                            context: './',
+                            outputPath: 'css',
+                            publicPath: ''
+                        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
                     {
                         loader: 'sass-loader',
                         options: {
@@ -26,10 +39,5 @@ module.exports = {
                 ]
             }
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/main.css'
-        })
-    ]
+    }
 };
