d89e36b59cdbc16cb9c50cf666d16fe669917a5a
Nick Pruitt <pruitt.nick@gmail.com>
2021-06-29 21:58:41 -0500

    Home page clean up and markup corrections

--- .eleventy.js
+++ .eleventy.js
@@ -15,7 +15,7 @@ module.exports = function(eleventyConfig) {
 
     // Current year for copyright
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-    eleventyConfig.addShortcode("staticFileCache", () => "0.0.5");
+    eleventyConfig.addShortcode("staticFileCache", () => "0.0.6");
 
     // Liquid options
     eleventyConfig.setLiquidOptions({
